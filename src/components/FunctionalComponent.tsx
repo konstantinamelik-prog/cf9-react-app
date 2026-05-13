db64bee29f4f199b52abc203bcbeb4c597669d5f
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-05-13 21:26:51 +0300

    feat: add class and functional components

---
+++ src/components/FunctionalComponent.tsx
@@ -0,0 +1,8 @@
+
+function FunctionalComponent() {
+    const title = "I am a Functional Component!"
+    return (
+        <h1 className="bg-red">{title}</h1>
+    );
+}
+export default FunctionalComponent;
