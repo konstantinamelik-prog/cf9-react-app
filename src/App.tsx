ac664ce4053c86e1b61a3fca63455b8d3899e88d
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-05-16 20:36:02 +0300

    feat: add NameChangerWithTitle component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@
 import Layout from "./components/Layout.tsx";
 // import CounterAdvanced from "./components/LessonFour/CounterAdvanced.tsx";
 // import NameChanger from "./components/LessonFive/NameChanger.tsx";
-import CounterWithCustomHook from "./components/LessonFive/CounterWithCustomHook.tsx";
+// import CounterWithCustomHook from "./components/LessonFive/CounterWithCustomHook.tsx";
+// import {useEffect} from "react";
+import NameChangerWithTitle from "./components/LessonFive/NameChangerWithTitle.tsx";
 // import Counter from "./components/LessonFour/Counter.tsx";
 // import ClassCounter from "./components/LessonFour/ClassCounter.tsx";
 
@@ -47,7 +49,18 @@ import CounterWithCustomHook from "./components/LessonFive/CounterWithCustomHook
 // }
 // // PropsThree = {title, description, price, sort, image}
 
+// function setup() {
+//     alert("count")
+// }
+//
+// useEffect(setup)
+
 function App() {
+
+    // useEffect(() => {
+    //     alert("Hello World!");
+    // }
+    // );
 
   return (
     <>
@@ -82,7 +95,8 @@ function App() {
         {/* LESSON FIVE */}
         <Layout addClasses="bg-gray-50">
             {/*<NameChanger/>*/}
-            <CounterWithCustomHook/>
+            {/*<CounterWithCustomHook/>*/}
+            <NameChangerWithTitle/>
         </Layout>
     </>
   )
