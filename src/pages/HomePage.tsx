36d57e9be5e4586652659c340b8bcd30b32a3f3a
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-05-28 20:11:35 +0300

    feat: add HomePage component

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+// import {Button} from "../components/ui/button.tsx";
+
+const HomePage = () => {
+    return (
+        <>
+            <h1 className="text-center">Home Page</h1>
+            {/*<Button>Button</Button>*/}
+        </>
+    )
+}
+export default HomePage;
