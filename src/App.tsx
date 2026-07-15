3f7dcbed971a117f0d2d79c7c7612e599713841c
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-15 20:00:51 +0300

    feat: add MultiFieldFormWithZod component

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import UserPage from "./pages/UserPage.tsx";
 import SearchPage from "./pages/SearchPage.tsx";
 import MultiFieldForm from "./components/LessonNine/MultiFieldForm.tsx";
 import MultiFieldFormWithValidation from "./components/LessonNine/MultiFieldFormWithValidation.tsx";
+import MultiFieldFormWithZod from "./components/LessonTen/MultiFieldFormWithZod.tsx";
 // import Counter from "./components/LessonFour/Counter.tsx";
 // import ClassCounter from "./components/LessonFour/ClassCounter.tsx";
 // import {useState} from "react";
@@ -170,7 +171,8 @@ function App() {
 
                     <Route path="multifield-form" element={<MultiFieldForm/>}/>
                     <Route path="multifield-form-validation" element={<MultiFieldFormWithValidation/>}/>
-
+                    <Route path="multifield-form-zod" element={<MultiFieldFormWithZod/>}/>
+                    {/*<Route path="multifield-form-hook-form" element={<MultiFieldFormWithHookForm/>}/>*/}
 
                 </Route>
                 {/* /files/* */}
