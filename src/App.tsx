d0b4a5580bb71d9686d8ba19d9b1b612931a8f84
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-07-15 21:11:55 +0300

    feat: add MultiFieldFormWithHookForm component using RHF and zod resolver

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ import SearchPage from "./pages/SearchPage.tsx";
 import MultiFieldForm from "./components/LessonNine/MultiFieldForm.tsx";
 import MultiFieldFormWithValidation from "./components/LessonNine/MultiFieldFormWithValidation.tsx";
 import MultiFieldFormWithZod from "./components/LessonTen/MultiFieldFormWithZod.tsx";
+import MultiFieldFormWithHookForm from "./components/LessonTen/MultiFieldFormWithHookForm.tsx";
 // import Counter from "./components/LessonFour/Counter.tsx";
 // import ClassCounter from "./components/LessonFour/ClassCounter.tsx";
 // import {useState} from "react";
@@ -172,7 +173,7 @@ function App() {
                     <Route path="multifield-form" element={<MultiFieldForm/>}/>
                     <Route path="multifield-form-validation" element={<MultiFieldFormWithValidation/>}/>
                     <Route path="multifield-form-zod" element={<MultiFieldFormWithZod/>}/>
-                    {/*<Route path="multifield-form-hook-form" element={<MultiFieldFormWithHookForm/>}/>*/}
+                    <Route path="multifield-form-hook-form" element={<MultiFieldFormWithHookForm/>}/>
 
                 </Route>
                 {/* /files/* */}
